6980589c8e2bcae8cc0132bbf01f758e88d9b80e
Tomi Dahab <tomidahab@gmail.com>
2023-07-06 20:07:28 -0300

    modifco lectura archivo

--- main.py
+++ main.py
@@ -16,7 +16,12 @@ class UploadFileForm(FlaskForm):
 
 def leer_info(ejs):
         
-    linea = ejs.readline()
+    linea = ejs.readline()        
+    print("\n\n\n")
+    
+    print(type(str((linea))))
+    print("\n\n\n")
+    linea = str(linea)
     linea = linea.replace(":","-")
     if linea and linea.count("-") >= 3:
         lista = linea.split("-")
@@ -27,27 +32,23 @@ def leer_info(ejs):
     return lista
     
 
-def chats():
+def chats_wpp(archivo):
     res = ""
-    path = os.getcwd()
-    path += "/static/files/out.txt"
-    with open(path, 'rt') as archivo:
-
+    datos = leer_info(archivo)
+    apellido = datos[2]
+    diccionario = {}
+    while apellido != "":
+        if apellido in diccionario:
+            diccionario[apellido] += 1
+        else:
+            diccionario[apellido] = 1
         datos = leer_info(archivo)
         apellido = datos[2]
-        diccionario = {}
-        while apellido != "":
-            if apellido in diccionario:
-                diccionario[apellido] += 1
-            else:
-                diccionario[apellido] = 1
-            datos = leer_info(archivo)
-            apellido = datos[2]
-        print(diccionario)
-        lista = sorted(diccionario.items(), key=lambda kv: kv[1], reverse = True)
-        for i in lista:
-            #print("{}: {} mensajes".format(i[0],i[1]))
-            res += "{}: {} mensajes\n".format(i[0],i[1])
+    print(diccionario)
+    lista = sorted(diccionario.items(), key=lambda kv: kv[1], reverse = True)
+    for i in lista:
+        #print("{}: {} mensajes".format(i[0],i[1]))
+        res += "{}: {} mensajes\n".format(i[0],i[1])
     return res
 
 
@@ -60,10 +61,10 @@ def home():
     form = UploadFileForm()
     if form.validate_on_submit():
         file = form.file.data # First grab the file
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename("out.txt"))) # Then save the file
+        #file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename("out.txt"))) # Then save the file
         #return "File has been uploaded."
-        print()
-        return render_template('result.html',res=chats())
+        file.seek(0)
+        return render_template('result.html',res=chats_wpp(file))
     return render_template('index.html', form=form)
 
 if __name__ == '__main__':
